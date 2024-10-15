dc2cef131d8fac121da34f3eb9bf6803b27fdbf3
Abby-Fan04 <yfan187@ucsb.edu>
2024-10-14 21:51:43 -0700

    fix teamtest and made sure everything worked perfectly

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -23,17 +23,23 @@ public class TeamTest {
     public void toString_returns_correct_string() {
         assertEquals("Team(name=test-team, members=[])", team.toString());
     }
+    @Test
+    public void toString_returns_correct_string_with_mem() {
+        Team team1 = new Team("test-team11");
+        team1.addMember("mem1");
+        assertEquals("Team(name=test-team11, members=[mem1])", team1.toString());
+    }
 
 //for equal()   
     @Test
     public void equal_case1() {
         Team team = new Team("test-team");    
-        assertEquals(true, team.equals(team););
+        assertEquals(true, team.equals(team));
     }
     @Test
     public void equal_case2() {
         Team team = new Team("test-team");
-        assertEquals(false, team.equals("team"););    
+        assertEquals(false, team.equals("team"));    
     }
     @Test
     public void equal_case3_TT() {
@@ -41,36 +47,36 @@ public class TeamTest {
         team1.addMember("mem1");
         Team team2 = new Team("test-team");   
         team2.addMember("mem1");
-        assertEquals(true, team1.equals(team2););
+        assertEquals(true, team1.equals(team2));
     }
     @Test
     public void equal_case3_TF() {
-        team1 = new Team("test-team");   
+        Team team1 = new Team("test-team");   
         team1.addMember("mem1");
-        team2 = new Team("test-team");   
+        Team team2 = new Team("test-team");   
         team2.addMember("mem2");
-        assertEquals(false, team1.equals(team2););
+        assertEquals(false, team1.equals(team2));
     }
     @Test
     public void equal_case3_FT() {
-        team1 = new Team("test-team11");   
+        Team team1 = new Team("test-team11");   
         team1.addMember("mem1");
-        team2 = new Team("test-team");   
+        Team team2 = new Team("test-team");   
         team2.addMember("mem1");
-        assertEquals(false, team1.equals(team2););
+        assertEquals(false, team1.equals(team2));
     }
     @Test
     public void equal_case3_FF() {
-        team1 = new Team("test-team11");   
+        Team team1 = new Team("test-team11");   
         team1.addMember("mem1");
-        team2 = new Team("test-team");   
+        Team team2 = new Team("test-team");   
         team2.addMember("mem2");
-        assertEquals(false, team1.equals(team2););
+        assertEquals(false, team1.equals(team2));
     }
 
 //test hash
     @Test
-    public void equal_case3_FT() {
+    public void test_hash_provided() {
         Team t1 = new Team();
         t1.setName("foo");
         t1.addMember("bar");
@@ -78,6 +84,13 @@ public class TeamTest {
         t2.setName("foo");
         t2.addMember("bar");
         assertEquals(t1.hashCode(), t2.hashCode());
+    }
+    @Test
+    public void test_hash_zero() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
     }
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
